2fa3b8e1e218724f4ec9973428492d27355c9eb6
Rakiubl-Islam-Sarker <72677836+Rakibul3316@users.noreply.github.com>
2021-04-18 14:06:02 +0600

    second commit

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 client.connect(err => {
     const servicesCollection = client.db("refitCompleteWebsite11").collection("addServices");
     const commentsCollection = client.db("refitCompleteWebsite11").collection("customerComments");
+    const ordersCollection = client.db("refitCompleteWebsite11").collection("orders");
 
     // Get the data form addservices(Admin)
     app.post('/addServices', (req, res) => {
@@ -37,18 +38,45 @@ client.connect(err => {
 
     // Read the data from database for our services area
     app.get('/services', (req, res) => {
-        servicesCollection.find()
+        servicesCollection.find({})
             .toArray((err, items) => {
                 res.send(items);
             })
     })
 
+    // Get the data form review(User)
     app.post('/addComments', (req, res) => {
         const customerComment = req.body;
         console.log(customerComment);
         commentsCollection.insertOne(customerComment)
             .then(result => {
                 res.send(result.insertedCount > 0)
+            })
+    })
+
+    // placed orders from the book(User)
+    app.post('/addOrders', (req, res) => {
+        const order = req.body;
+        console.log(order);
+        ordersCollection.insertOne(order)
+            .then(result => {
+                res.send(result.insertedCount > 0)
+            })
+    })
+
+
+    app.get('/userOrders', (req, res) => {
+        ordersCollection.find({ userEmail: req.query.email })
+            .toArray((err, items) => {
+                res.send(items);
+            })
+    })
+
+
+    app.get('/allOrders', (req, res) => {
+        ordersCollection.find({})
+            .toArray((err, itmes) => {
+                res.send(itmes)
             })
     })
 });
